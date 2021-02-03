84a80ccfb1310509cce375eb931af9e457b0f2b6
Ifrah Saleem <ifrahsaleem@sabanciuniv.edu>
2021-02-03 22:39:33 +0500

    components in place

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-@Component({
+@Component({ 
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <app-customers></app-customers>
+  `
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements OnInit {
+ 
+  constructor() { }
+
+  ngOnInit() {
+    // We call a service that gets us the data
+
+  }
+  
 }
