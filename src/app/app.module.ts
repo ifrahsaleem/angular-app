d63c0228140bf1e5bd258e3f4e9dc25369d8aa7f
Ifrah Saleem <ifrahsaleem@sabanciuniv.edu>
2021-02-12 00:46:57 +0500

    routing customer module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,10 @@ import { CoreModule } from './core/core.module';
 import { CustomersModule } from './customers/customers.module';
 import {SharedModule } from './shared/shared.module';
 import { AppComponent }  from './app.component';
-
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  imports:      [ BrowserModule, CoreModule, CustomersModule, SharedModule],
+  imports:      [ BrowserModule, CoreModule, CustomersModule, SharedModule, AppRoutingModule],
   declarations: [ AppComponent],
   bootstrap:    [ AppComponent ]
 })
--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { FormsModule }      from '@angular/forms';
 import {SharedModule } from '../shared/shared.module';
 
+import { CustomersRoutingModule } from './customers-routing.module';
 import { CustomersComponent }  from './customers.component';
 import { CustomersListComponent }  from './customers-list/customers-list.component';
 import { FilterTextboxComponent } from './customers-list/filter-textbox/filter-textbox.component';
 @NgModule({
-  imports:      [ CommonModule, SharedModule, FormsModule ],
+  imports:      [ CommonModule, SharedModule, FormsModule, CustomersRoutingModule ],
   declarations: [ CustomersComponent, CustomersListComponent, FilterTextboxComponent],
   exports: [ CustomersComponent]
 })
